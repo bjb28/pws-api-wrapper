e6f052aeb2d62821ff7a7d464b3e09493bb0ccaa
Bryce <swarvingprogrammer@gmail.com>
2022-06-10 14:45:29 -0400

    Checks initial validation fails.

--- tests/test_scratchpads.py
+++ tests/test_scratchpads.py
@@ -19,3 +19,30 @@ class TestScratchpad:
         scratchpad = Scratchpad(**scratchpad_dict)
 
         assert scratchpad.to_dict() == scratchpad_dict
+
+    @pytest.mark.parametrize(
+        "attribute,value,error_message",
+        [
+            ("id", 4, '"id" should be 8 alphanumeric characters'),
+            ("id", "asd123", '"id" should be 8 alphanumeric characters'),
+            ("id", "abcd123!", '"id" should be 8 alphanumeric characters'),
+            ("hid", 4, '"hid" should be 8 alphanumeric characters'),
+            ("hid", "asd123", '"hid" should be 8 alphanumeric characters'),
+            ("hid", "abcd123!", '"hid" should be 8 alphanumeric characters'),
+            ("title", "", 'Scratchpad "title" is required.'),
+            ("language", "green", 'language" should be None or one of the following:'),
+            ("id", 4, '"id" should be 8 alphanumeric characters'),
+            ("id", "asd123", '"id" should be 8 alphanumeric characters'),
+            ("id", "abcd123!", '"id" should be 8 alphanumeric characters'),
+            ("content", 1, '"contented" should be a string or None.'),
+            ("type", "green", '"type" should be None or one of the following:'),
+            ("type", 1, '"type" should be None or one of the following:'),
+        ],
+    )
+    def test_init_validation_fail(
+        self, attribute, value, error_message, scratchpad_dict
+    ):
+        """Test the init validation fails when string value are not strings."""
+        scratchpad_dict[attribute] = value
+        with pytest.raises(SchemaError, match=error_message):
+            Scratchpad(**scratchpad_dict)
