a0e56e8ec7d2925b8e1b39abe94b1efc801fec8e
Bryce <swarvingprogrammer@gmail.com>
2022-10-05 21:57:37 -0400

    Adds create function to Note Pages.

--- src/pws_api_wrapper/notePage.py
+++ src/pws_api_wrapper/notePage.py
@@ -1,11 +1,14 @@
 """NotePage Object."""
 
 # Standard Python Libraries
+import json
 import re
 import sys
 from typing import Any
 
 # Third-Party Libraries
+from requests.models import Response
+from schema import And, Optional, Or, Regex, Schema, SchemaError
 
 # Customer Libraries
 from .abstract_endpoint import AbstractEndpoint
@@ -37,6 +40,7 @@ class NotePage(AbstractEndpoint):
                     Regex(r"^[a-zA-Z0-9]{8,}$", flags=re.IGNORECASE),
                     error='"id" should be 8 alphanumeric characters',
                 ),
+                # TODO Make object_id and object_type both required when one is present.
                 Optional("object_id"): And(
                     str,
                     Regex(r"^[a-zA-Z0-9]{8,}$", flags=re.IGNORECASE),
@@ -70,3 +74,38 @@ class NotePage(AbstractEndpoint):
 
         for key, value in validated_args.items():
             setattr(self, key, value)
+
+        try:
+            # If a notePad ID, object_id, and object_type is provided, creates the notePad_path.
+            self.notepad_path: str = f"{AbstractEndpoint.path}/{self.object_type}/{self.object_id}/notepages/{self.id}"
+        except AttributeError:
+            pass
+
+        if self.object_id and self.object_type:
+            # Creates and object_path if object_id and object_type are provided.
+            self.object_path: str = (
+                f"{AbstractEndpoint.path}/{self.object_type}/{self.object_id}/notepages"
+            )
+
+    def create(self) -> str:
+        """Create a Note Pad in pentest.ws."""
+        self.pws_session.headers["Content-Type"] = "application/json"
+
+        notepad_dict: dict = self.to_dict()
+
+        notepad_data: str = json.dumps(notepad_dict)
+
+        # TODO Custom Exception (Issue 1)
+        response: Response = self.pws_session.post(
+            self.object_path, headers=self.pws_session.headers, data=notepad_data
+        )
+
+        # TODO Custom Exception (Issue 1)
+        if response.status_code == 200:
+            self.id = response.json()["id"]
+            # FIXME The next line is flagged by mypy for NotePage not having an attribute "title".
+            message: str = f"Note Page {self.title} ({self.id}) created."  # type: ignore
+        elif response.status_code == 400:
+            message = f"Error: {response.json()['msg']}"
+
+        return message
