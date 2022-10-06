fe1a5973c0f85d4c37d05220a908b4d6e98845ae
Bryce <swarvingprogrammer@gmail.com>
2022-10-05 21:57:01 -0400

    Builds tests for creating a Note Page.

--- tests/test_notePages.py
+++ tests/test_notePages.py
@@ -13,6 +13,36 @@ from pws_api_wrapper import NotePage
 class TestNotePage:
     """Tests for the NotePage."""
 
+    @vcr.use_cassette("tests/vcr_cassettes/notePage/create-200.yml")
+    def test_create_200(self, notePage_dict):
+        """Test an API call to create a Note Page."""
+        # Delete id as teh API will not accept when creating.
+        del notePage_dict["id"]
+
+        notePage = NotePage(**notePage_dict)
+
+        message = notePage.create()
+
+        # Add note page ID back to notePage_dict
+        notePage_dict["id"] = notePage.id
+
+        assert isinstance(notePage, NotePage)
+        assert notePage.to_dict() == notePage_dict
+        assert message == "Note Page Engagement Test (96ZKAyx2) created."
+
+    @vcr.use_cassette("tests/vcr_cassettes/notePage/create-400.yml")
+    def test_create_400(self, notePage_dict):
+        """Test an API call to fail at creating a Note Page."""
+        # Add a fake engagement id to cause an error.
+        notePage_dict["object_id"] = "12345678"
+
+        notePage = NotePage(**notePage_dict)
+
+        message = notePage.create()
+
+        assert isinstance(notePage, NotePage)
+        assert message == "Error: Invalid engagements ID"
+
     def test_init_validation_pass(self, notePage_dict):
         """Test the init validation."""
         notePage = NotePage(**notePage_dict)
