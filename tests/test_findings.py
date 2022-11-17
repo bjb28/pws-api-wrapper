e06c80bdaf3bddd823bd2d263e9e495416b1acdc
Bryce <swarvingprogrammer@gmail.com>
2022-11-16 22:34:20 -0500

    Adds test for create method.

--- tests/test_findings.py
+++ tests/test_findings.py
@@ -11,6 +11,24 @@ from pws_api_wrapper import Finding
 class TestFinding:
     """Tests for the Finding Endpoint."""
 
+    @vcr.use_cassette("tests/vcr_cassettes/finding/create-200.yml")
+    def test_create_200(self, finding_dict):
+        """Test an API call to create a Finding."""
+        # Delete id as teh API will not accept when creating.
+        del finding_dict["id"]
+
+        finding = Finding(**finding_dict)
+
+        message = finding.create()
+
+        # Add finding ID back to finding_dict
+        finding_dict["id"] = finding.id
+
+        assert isinstance(finding, Finding)
+        assert finding.to_dict() == finding_dict
+        assert message == "Finding Test Finding (nDa0BVxK) created."
+
+    def test_init_validation_pass(self, finding_dict):
         """Test the init validation."""
         notePage = Finding(**finding_dict)
 
